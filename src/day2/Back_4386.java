5f8acaef2acc12ddc60ca6f670c6aaa2b38b2540
Your ID <your email address>
2023-12-20 17:51:56 +0900

    문제 풀이 다르게 적용

---
+++ src/day2/Back_4386.java
@@ -0,0 +1,113 @@
+package day2;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Back_4386 {
+	
+	private static int n;
+	private static double [][] stars;
+	private static PriorityQueue<Star> pq;
+	private static int[] parents;
+	
+	private static class Star implements Comparable<Star>{
+		int s,e;
+		double x1,x2,y1,y2,dis;
+		
+		Star(int s, int e,double x1, double x2, double y1, double y2, double dis){
+			this.s = s;
+			this.e = e;
+			this.x1 = x1;
+			this.x2 = x2;
+			this.y1 = y1;
+			this.y2 = y2;
+			this.dis = dis;
+		}
+		
+		@Override
+		public int compareTo(Star o) {
+			if(this.dis > o.dis) return 1;
+			else if(this.dis < o.dis) return -1;
+			else return 0;
+		}
+	}
+	
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer stz;
+		
+		n = Integer.parseInt(br.readLine());
+		stars = new double[n+1][2];
+		parents = new int[n+1];
+		
+		for(int i = 0; i< n + 1; i++) {
+			stz = new StringTokenizer(br.readLine()," ");
+			stars[i][0] = Double.parseDouble(stz.nextToken());
+			stars[i][1] = Double.parseDouble(stz.nextToken());
+			
+			parents[i] = i;
+		}
+		
+		pq = new PriorityQueue<>();
+		
+		for(int i = 1; i < n + 1; i++) {
+			for(int j = i+1; j<n+1;j++) {
+				double x1 = stars[i][0];
+				double y1 = stars[i][1];
+				
+				double x2 = stars[j][0];
+				double y2 = stars[j][1];
+				
+				double x = Math.pow(x1 - x2, 2);
+				double y = Math.pow(y1-y2, 2);
+				
+				double dis = Math.sqrt(x+y);
+				
+				pq.add(new Star(i,j,x1,x2,y1,y2,dis));
+			}
+		}
+		
+		double total = 0.0;
+		int cnt = n-1;
+		while(!pq.isEmpty() && cnt > 0) {
+			Star star = pq.poll();
+			
+			if(union(star.s,star.e)) {
+				total += star.dis;
+				cnt--;
+			}
+		}
+		
+		double ans = (double)Math.round(total*100)/100;
+		bw.write(String.valueOf(ans));
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+	
+	private static boolean union(int a, int b) {
+		int root1 = find(a);
+		int root2 = find(b);
+		
+		if(root1 == root2) return false;
+		if(root2 > root1) parents[root2] = root1;
+		else parents[root1] = root2;
+		
+		return true;
+	}
+	private static int find(int root) {
+		if(parents[root] == root) {
+			return root;
+		}
+		return find(parents[root]);
+	}
+
+}
